9161ecefcc53f197d47b96d671a683dd461850f7
vozle <vozle2@gmail.com>
2022-03-24 23:46:35 +0300

    v2.0

---
+++ src/main/java/ru/netology/repository/MovieRepository.java
@@ -0,0 +1,46 @@
+package ru.netology.repository;
+
+import ru.netology.domain.Movie;
+
+public class MovieRepository {
+    private Movie[] movies = new Movie[0];
+
+    public Movie[] findAll() {
+        return movies;
+    }
+
+    public void save(Movie movie) {
+        int length = movies.length + 1;
+        Movie[] tmp = new Movie[length];
+        System.arraycopy(movies, 0, tmp, 0, movies.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = movie;
+        movies = tmp;
+    }
+
+    public Movie findById(int id) {
+        for (Movie movie : movies) {
+            if (movie.getId() == id) {
+                return movie;
+            }
+        }
+        return null;
+    }
+
+    public Movie[] removeById(int id) {
+        int length = movies.length - 1;
+        Movie[] tmp = new Movie[length];
+        int index = 0;
+        for (Movie movie : movies) {
+            if (movie.getId() != id) {
+                tmp[index] = movie;
+                index++;
+            }
+        }
+        return movies = tmp;
+    }
+
+    public void removeAll() {
+        movies = new Movie[0];
+    }
+}
--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -1,63 +1,32 @@
 package ru.netology.manager;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import ru.netology.domain.Movie;
+import ru.netology.repository.MovieRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.mockito.Mockito.doReturn;
 
-class MovieManagerTest {
+class MovieManagerTest<expected> {
+    @Mock
+    private MovieRepository repository;
+    @InjectMocks
+    private MovieManager manager = new MovieManager(repository);
+    private Movie first = new Movie(1, "Бладшот", "боевик");
+    private Movie second = new Movie(2, "Вперёд", "мультфильм");
+    private Movie third = new Movie(3, "Отель 'Белград'", "комедия");
+    private Movie fourth = new Movie(4, "Джентельмены", "боевик");
+    private Movie fifth = new Movie(5, "Человек-невидимка", "ужасы");
+    private Movie sixth = new Movie(6, "Тролли. Мировой тур", "мультфильм");
+    private Movie seventh = new Movie(7, "Номер один", "комедия");
 
-    @Test
-    public void shouldSaveNewMovieIfNotExists() {
-        MovieManager manager = new MovieManager();
-        Movie first = new Movie(1, "Бладшот", "боевик");
+    Movie[] expected = {seventh, sixth, fifth, fourth, third, second, first};
 
-        manager.addNew(first);
-
-        Movie[] actual = manager.findAll();
-        Movie[] expected = {first};
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSaveNewMovieIfAlreadyContains() {
-        MovieManager manager = new MovieManager();
-        Movie first = new Movie(1, "Бладшот", "боевик");
-        Movie second = new Movie(2, "Вперёд", "мультфильм");
-        Movie third = new Movie(3, "Отель 'Белград'", "комедия");
-
-        manager.addNew(first);
-        manager.addNew(second);
-
-        manager.addNew(third);
-
-        Movie[] actual = manager.findAll();
-        Movie[] expected = {first, second, third};
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldFindAllMoviesIfNotExists() {
-        MovieManager manager = new MovieManager();
-        Movie[] actual = manager.findAll();
-        Movie[] expected = {};
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldFindAllMovies() {
-        MovieManager manager = new MovieManager();
-        Movie first = new Movie(1, "Бладшот", "боевик");
-        Movie second = new Movie(2, "Вперёд", "мультфильм");
-        Movie third = new Movie(3, "Отель 'Белград'", "комедия");
-        Movie fourth = new Movie(4, "Джентельмены", "боевик");
-        Movie fifth = new Movie(5, "Человек-невидимка", "ужасы");
-        Movie sixth = new Movie(6, "Тролли. Мировой тур", "мультфильм");
-        Movie seventh = new Movie(7, "Номер один", "комедия");
-
+    @BeforeEach
+    void shouldAdd() {
         manager.addNew(first);
         manager.addNew(second);
         manager.addNew(third);
@@ -65,89 +34,18 @@ class MovieManagerTest {
         manager.addNew(fifth);
         manager.addNew(sixth);
         manager.addNew(seventh);
-
-        Movie[] actual = manager.findAll();
-        Movie[] expected = {first, second, third, fourth, fifth, sixth, seventh};
-
-        assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldFindAllMoviesWithLimit5() {
-        MovieManager manager = new MovieManager(5);
+    public void shouldAddMovie() {
+        Movie[] returned = new Movie[]{first, second, third, fourth};
+        doReturn(returned).when(repository).findAll();
 
-        Movie first = new Movie(1, "Бладшот", "боевик");
-        Movie second = new Movie(2, "Вперёд", "мультфильм");
-        Movie third = new Movie(3, "Отель 'Белград'", "комедия");
-        Movie fourth = new Movie(4, "Джентельмены", "боевик");
-        Movie fifth = new Movie(5, "Человек-невидимка", "ужасы");
-        Movie sixth = new Movie(6, "Тролли. Мировой тур", "мультфильм");
-        Movie seventh = new Movie(7, "Номер один", "комедия");
-
-        manager.addNew(first);
-        manager.addNew(second);
-        manager.addNew(third);
-        manager.addNew(fourth);
         manager.addNew(fifth);
-        manager.addNew(sixth);
-        manager.addNew(seventh);
 
-        Movie[] actual = manager.findAll();
+        Movie[] actual = manager.findLast();
         Movie[] expected = {first, second, third, fourth, fifth};
 
         assertArrayEquals(expected, actual);
     }
-
-    @Test
-    public void shouldFindLastAdded() {
-        MovieManager manager = new MovieManager();
-
-        Movie first = new Movie(1, "Бладшот", "боевик");
-        Movie second = new Movie(2, "Вперёд", "мультфильм");
-        Movie third = new Movie(3, "Отель 'Белград'", "комедия");
-        Movie fourth = new Movie(4, "Джентельмены", "боевик");
-        Movie fifth = new Movie(5, "Человек-невидимка", "ужасы");
-        Movie sixth = new Movie(6, "Тролли. Мировой тур", "мультфильм");
-        Movie seventh = new Movie(7, "Номер один", "комедия");
-
-        manager.addNew(first);
-        manager.addNew(second);
-        manager.addNew(third);
-        manager.addNew(fourth);
-        manager.addNew(fifth);
-        manager.addNew(sixth);
-        manager.addNew(seventh);
-
-        Movie[] actual = manager.findLast();
-        Movie[] expected = {seventh, sixth, fifth, fourth, third, second, first};
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldFindLastAddedWithLimit3() {
-        MovieManager manager = new MovieManager(3);
-
-        Movie first = new Movie(1, "Бладшот", "боевик");
-        Movie second = new Movie(2, "Вперёд", "мультфильм");
-        Movie third = new Movie(3, "Отель 'Белград'", "комедия");
-        Movie fourth = new Movie(4, "Джентельмены", "боевик");
-        Movie fifth = new Movie(5, "Человек-невидимка", "ужасы");
-        Movie sixth = new Movie(6, "Тролли. Мировой тур", "мультфильм");
-        Movie seventh = new Movie(7, "Номер один", "комедия");
-
-        manager.addNew(first);
-        manager.addNew(second);
-        manager.addNew(third);
-        manager.addNew(fourth);
-        manager.addNew(fifth);
-        manager.addNew(sixth);
-        manager.addNew(seventh);
-
-        Movie[] actual = manager.findLast();
-        Movie[] expected = {seventh, sixth, fifth};
-
-        assertArrayEquals(expected, actual);
-    }
-
 }
